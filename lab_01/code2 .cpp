6873dce9b9587a9552329ecaffc54cb32cff53eb
shishir mondal <128738233+shishirmondal13@users.noreply.github.com>
2023-07-30 14:58:44 +0600

    Add files via upload

---
+++ lab_01/code2 .cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello in my new university"<<endl<<endl;
+
+    cout<<"Im a student in it faculty"<<endl<<endl;
+    cout<<"Welcome";
+}
