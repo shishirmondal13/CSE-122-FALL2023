e981091a4bcd663fef6b66cfa9afcf4afda16484
shishir mondal <128738233+shishirmondal13@users.noreply.github.com>
2023-07-30 16:12:59 +0600

    Add files via upload

---
+++ lab_01/code3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2,num3;
+    cout << "Enter three numbers: ";
+    cin >> num1 >> num2 >> num3;
+    if(num1 >= num2 && num1 >= num3)
+        cout << "Largest number: " << num1;
+
+    else if(num2 >= num1 && num2 >= num3)
+        cout << "Largest number: " << num2;
+
+    else
+        cout << "Largest number: " << num3;
+
+
+
+}
