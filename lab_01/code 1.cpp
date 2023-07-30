e9f72cce4dba76d00ab226be15b77a53c60215ac
shishir mondal <128738233+shishirmondal13@users.noreply.github.com>
2023-07-30 15:53:49 +0600

    Add files via upload

---
+++ lab_01/code 1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int  main()
+{
+  cout<<"welcome to the wonderful world of c++!!!\n";
+  cout<<"Lab1\t";
+  cout<<"\n END\tPROGRAM\tBYE";
+  cout<<"WELCOME in yarmouk university\rHellooo";
+
+}
