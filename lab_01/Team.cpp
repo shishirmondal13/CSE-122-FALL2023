d830931b2374ad7f9562720c7f1f36ab75d9a443
shishir mondal <128738233+shishirmondal13@users.noreply.github.com>
2023-08-06 14:37:39 +0600

    Update Team.cpp

--- lab_01/Team.cpp
+++ lab_01/Team.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main()
 {
-    int n, Petya, Vasya, Tonya, number(0);
+    int n, Petya, Vasya, Tonya, number=0;
     cin >> n;
     while (n--)
     {
         cin >> Petya >> Vasya >> Tonya;
         if (Petya + Vasya + Tonya >= 2)
         {
-            number += 1;
+            number ++;
         }
     }
     cout << number << endl;
